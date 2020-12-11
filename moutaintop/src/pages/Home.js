d525984402300fdb44780dd54f0c1c2df301a326
Your Name <alexsigala87@gmail.com>
2020-12-11 00:02:20 -0800

    added nav bar which will emulate voltron header

--- moutaintop/src/pages/Home.js
+++ moutaintop/src/pages/Home.js
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
 
 import Jumbotron from "../components/Jumbotron";
 class Home extends Component {
     render() {
         return (
             <div>
+                <>
+                    <Navbar bg="light">
+                        <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+                    </Navbar>
+                    <br />
+                    <Navbar bg="light">
+                        <Navbar.Brand>Brand text</Navbar.Brand>
+                    </Navbar>
+                    <br />
+                    <Navbar bg="dark">
+                        <Navbar.Brand href="#home">
+                            <img
+                                src="/logo.svg"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                                alt="React Bootstrap logo"
+                            />
+                        </Navbar.Brand>
+                    </Navbar>
+                    <br />
+                    <Navbar bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">
+                            <img
+                                alt=""
+                                src="/logo.svg"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{" "}
+                            React Bootstrap
+                        </Navbar.Brand>
+                    </Navbar>
+                </>
                 <div>
                     <Jumbotron
                         title="Mountain Top Grails"
